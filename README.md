963c43d01927447928b549a3377d3a92d531b825
Muhammad Lukman Hakim <106156702+lukmanhakimcs@users.noreply.github.com>
2022-05-24 18:40:55 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## About This Project (release-and-work-in-progress 👷🔧️👷‍♀️⛏)
+[![pages-build-deployment](https://github.com/amirisback/amirisback.github.io/actions/workflows/pages/pages-build-deployment/badge.svg)](https://github.com/amirisback/amirisback.github.io/actions/workflows/pages/pages-build-deployment)
+- Web Profile Faisal Amir
+
+## Colaborator
+Very open to anyone, I'll write your name under this, please contribute by sending an email to me
+
+- Mail To faisalamircs@gmail.com
+- Subject : Github _ [Github-Username-Account] _ [Language] _ [Repository-Name]
+- Example : Github_amirisback_kotlin_admob-helper-implementation
+
+Name Of Contribute
+- Muhammad Faisal Amir
+- Waiting List
+- Waiting List
+
+Waiting for your contribute
+
+## Attention !!!
+- Please enjoy and don't forget fork and give a star
+- Don't Forget Follow My Github Account
---
+++ res/js/values/strings.js
@@ -0,0 +1,64 @@
+var web_name = "Muhammad Faisal Amir";
+
+var text_name  = "Muhammad Faisal Amir";
+var text_job  = "Android Programmer";
+var text_copyright  = "Copyright &copy Frogobox 2019";
+var text_portofolio  = "Amir's Web Profile";
+
+var text_app_linkedin   = "LinkedIn";
+var text_app_facebook   = "Facebook";
+var text_app_instagram  = "Instagram";
+var text_app_medium     = "Medium";
+var text_app_dribbble   = "Dribbble";
+var text_app_playstore   = "Play Store";
+var text_app_github   = "Github";
+
+var text_link_linkedin   = "linkedin.com/in/faisalamircs";
+var text_link_facebook   = "facebook.com/faisalamircs";
+var text_link_instagram  = "instagram.com/faisalamircs";
+var text_link_medium     = "medium.com/@faisalamircs";
+var text_link_dribbble   = "dribbble.com/faisalamircs";
+var text_link_playstore   = "FrogoBox";
+var text_link_github   = "github.com/amirisback";
+
+var text_edu_high_school_level = "High School";
+var text_edu_high_school_institute = "SMAN 1 Probolinggo";
+var text_edu_high_school_year = "2013 - 2016";
+
+var text_edu_diploma_level = "Diploma";
+var text_edu_diploma_institute = "Telkom University";
+var text_edu_diploma_major = "Informatic Engineering";
+var text_edu_diploma_year = "2016 - 2019";
+
+var product_name_cat = "Cat App";
+var product_name_finpro = "Finpro Management";
+var product_name_football = "Football App";
+var product_name_frogonews = "FrogoNews App";
+var product_name_jagosholat = "Jago Sholat";
+var product_name_amir = "Muhammad Faisal Amir";
+var product_name_jami = "Jami";
+var product_name_mood = "Mood";
+var product_name_movie = "Movie App";
+var product_name_romis = "Romis";
+var product_name_shejek = "Shejek";
+
+var product_name_deknappe = "De-Knappe Web";
+var product_name_misaiwan = "Misaiwan Web";
+var product_name_promadika = "Promadika Desktop";
+var product_name_sinop = "Sinop Desktop";
+
+var title_social_media = "Social Media";
+var title_education = "Education";
+var title_skill = "Skill";
+var title_product = "Product";
+var title_title = "Title";
+var title_about_me = "About Me";
+
+var subtitle_subtitle = "Subtitle";
+var subtitle_social_media = "Follow my social media account";
+var subtitle_education = "My background education";
+var subtitle_skill = "My professional skill";
+var subtitle_product = "My list product that I make it";
+
+var text_desc_me = "Muhammad Faisal Amir, lulusan D3 Teknik Informatika Telkom University, sangat tertarik dalam bidang pemrograman Android, suka menulis di medium dan aktif mendesign social media";
+var text_desc_skill = "Saya memiliki tanggung jawab dedikasi tinggi, kerja keras, dan memiliki keterampilan komunikasi yang baik. Saya dapat menggunakan Java, Kotlin, Restfull API Laravel, cepat dalam membuat UI xml. Saya ahli dalam menggunakan Android Studio IDE, membuat dokumentasi pengkodean yang baik untuk tim saya, pandai mengatur proyek.";
